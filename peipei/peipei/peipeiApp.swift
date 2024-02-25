c19dea51a48aee240294519391405173d12c2c3b
Sanjay Salem <sanjaysalem17@gmail.com>
2024-02-24 17:56:47 -0800

    add xcodeproject

---
+++ peipei/peipei/peipeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  peipeiApp.swift
+//  peipei
+//
+//  Created by Sanjay Salem on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct peipeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
